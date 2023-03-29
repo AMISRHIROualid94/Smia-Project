309e68d6b3fba08b793a3fbd06cb9d0e991ebec1
AMISRHIROualid94 <56658551+AMISRHIROualid94@users.noreply.github.com>
2023-03-29 03:47:21 +0000

    Add Organization service and configure it with eureka server.

---
+++ organization-service/src/main/java/com/optimagrowth/organization/model/Organization.java
@@ -0,0 +1,30 @@
+package com.optimagrowth.organization.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "organizations")
+public class Organization {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @Column(name = "name", nullable = false)
+    String name;
+
+    @Column(name = "contact_name", nullable = false)
+    String contactName;
+
+    @Column(name = "contact_email", nullable = false)
+    String contactEmail;
+
+    @Column(name = "contact_phone", nullable = false)
+    String contactPhone;
+}
